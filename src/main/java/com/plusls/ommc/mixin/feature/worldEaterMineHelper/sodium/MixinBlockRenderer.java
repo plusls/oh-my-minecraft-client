8e971d1c8c65cf60990eb4ad11a11fc25ce2575e
plulsls <plusls@qq.com>
2021-03-09 01:42:18 +0800

    add support for sodium

---
+++ src/main/java/com/plusls/ommc/mixin/feature/worldEaterMineHelper/sodium/MixinBlockRenderer.java
@@ -0,0 +1,45 @@
+package com.plusls.ommc.mixin.feature.worldEaterMineHelper.sodium;
+
+import com.plusls.ommc.feature.worldEaterMineHelper.BlockModelRendererContext;
+import com.plusls.ommc.feature.worldEaterMineHelper.CustomBakedModels;
+import me.jellysquid.mods.sodium.client.render.pipeline.BlockRenderer;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockRenderView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Coerce;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = BlockRenderer.class, remap = false)
+public class MixinBlockRenderer {
+    private final ThreadLocal<BlockModelRendererContext> ommcRenderContext = ThreadLocal.withInitial(BlockModelRendererContext::new);
+
+    // @Coerce Object
+    // stable -> ModelQuadSinkDelegate builder
+    // next -> ChunkModelBuffers buffers
+
+    @Inject(method = "renderModel",
+            at = @At(value = "HEAD"))
+    private void initRenderContext(BlockRenderView world, BlockState state, BlockPos pos, BakedModel model, @Coerce Object object, boolean cull, long seed, CallbackInfoReturnable<Boolean> cir) {
+        BlockModelRendererContext context = ommcRenderContext.get();
+        context.pos = pos;
+        context.state = state;
+    }
+
+    @ModifyVariable(method = "renderModel",
+            at = @At(value = "HEAD"), ordinal = 0)
+    private BakedModel modifyBakedModel(BakedModel bakedModel) {
+        BlockModelRendererContext context = ommcRenderContext.get();
+        Block block = context.state.getBlock();
+        if (CustomBakedModels.shouldUseCustomModel(block, context.pos)) {
+            return CustomBakedModels.models.get(block);
+        } else {
+            return bakedModel;
+        }
+    }
+}
