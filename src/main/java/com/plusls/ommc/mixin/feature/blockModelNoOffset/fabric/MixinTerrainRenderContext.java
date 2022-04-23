a7edf884a683a5a9bbea65c90b4afdba54d598bf
Hendrix-Shen <HendrixShen@hendrixshen.top>
2022-04-23 19:03:13 +0800

    Compat fabric-renderer-indigo 0.5.0

--- src/main/java/com/plusls/ommc/mixin/feature/blockModelNoOffset/fabric/MixinTerrainRenderContext.java
+++ src/main/java/com/plusls/ommc/mixin/feature/blockModelNoOffset/fabric/MixinTerrainRenderContext.java
@@ -9,6 +9,7 @@ import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -18,12 +19,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = TerrainRenderContext.class, remap = false)
 public abstract class MixinTerrainRenderContext implements RenderContext {
-
     @Final
     @Shadow
     private TerrainBlockRenderInfo blockInfo;
 
-    @Inject(method = "tesselateBlock", at = @At(value = "HEAD"))
+    @Dynamic
+    @Inject(method = {
+            "tessellateBlock", // For fabric-renderer-indigo 0.5.0 and above
+            "tesselateBlock" // For fabric-renderer-indigo 0.5.0 below
+    }, at = @At(value = "HEAD"), require = 0)
     private void blockModelNoOffset(BlockState blockState, BlockPos blockPos, BakedModel model, MatrixStack matrixStack, CallbackInfoReturnable<Boolean> cir) {
         Vec3d offsetPos = blockState.getModelOffset(blockInfo.blockView, blockPos);
         if (BlockModelNoOffsetUtil.shouldNoOffset(blockState)) {
--- src/main/java/com/plusls/ommc/mixin/feature/worldEaterMineHelper/fabric/MixinTerrainRenderContext.java
+++ src/main/java/com/plusls/ommc/mixin/feature/worldEaterMineHelper/fabric/MixinTerrainRenderContext.java
@@ -8,6 +8,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -22,8 +23,12 @@ public abstract class MixinTerrainRenderContext implements RenderContext {
     @Shadow
     private TerrainBlockRenderInfo blockInfo;
 
-    @Inject(method = "tesselateBlock", at = @At(value = "INVOKE",
-            target = "Lnet/fabricmc/fabric/api/renderer/v1/model/FabricBakedModel;emitBlockQuads(Lnet/minecraft/world/BlockRenderView;Lnet/minecraft/block/BlockState;Lnet/minecraft/util/math/BlockPos;Ljava/util/function/Supplier;Lnet/fabricmc/fabric/api/renderer/v1/render/RenderContext;)V", shift = At.Shift.AFTER, ordinal = 0, remap = true))
+    @Dynamic
+    @Inject(method = {
+            "tessellateBlock", // For fabric-renderer-indigo 0.5.0 and above
+            "tesselateBlock" // For fabric-renderer-indigo 0.5.0 below
+    }, at = @At(value = "INVOKE",
+            target = "Lnet/fabricmc/fabric/api/renderer/v1/model/FabricBakedModel;emitBlockQuads(Lnet/minecraft/world/BlockRenderView;Lnet/minecraft/block/BlockState;Lnet/minecraft/util/math/BlockPos;Ljava/util/function/Supplier;Lnet/fabricmc/fabric/api/renderer/v1/render/RenderContext;)V", shift = At.Shift.AFTER, ordinal = 0, remap = true), require = 0)
     private void emitCustomBlockQuads(BlockState blockState, BlockPos blockPos, BakedModel model, MatrixStack matrixStack, CallbackInfoReturnable<Boolean> cir) {
         WorldEaterMineHelperUtil.emitCustomBlockQuads(blockInfo.blockView, blockInfo.blockState, blockInfo.blockPos, blockInfo.randomSupplier, this);
     }
